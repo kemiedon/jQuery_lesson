89ad28db093ad8235f9ecc6019b345a4329216cf
kemiedon <kemiedon@gmail.com>
2023-06-25 16:23:41 +0800

    2023-06-25 v1.3
    新增face cursor.js 外掛範例
    03_Effects的open_card範例

---
+++ 03_Effects/js/open_card.js
@@ -0,0 +1,24 @@
+$(function(){
+    $(".openB").click(function(){
+      $(".left").addClass("open");
+      setTimeout(function(){
+        $(".right").addClass("open");
+      }, 250);
+      setTimeout(function(){
+        $(".back").addClass("open");
+      }, 350);
+      $(".closeB").delay(1000).fadeIn();
+    });
+  
+    $(".closeB").click(function(){
+      setTimeout(function(){
+        $(".left").removeClass("open");
+      }, 250);
+      $(".right").removeClass("open");
+      setTimeout(function(){
+        $(".back").removeClass("open");
+      }, 600);
+      $(".closeB").fadeOut();
+    });
+  });
+  
